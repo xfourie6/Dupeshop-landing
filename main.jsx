bf2f0e9f129c2cf982a2d1151241bf820a028d1b
xfourie6 <xfourie6@gmail.com>
2026-05-08 11:28:50 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DupeshopLandingPage from '../dupeshop_za_landing_page'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DupeshopLandingPage />
+  </React.StrictMode>,
+)
