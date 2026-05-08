cb8e1ba40481f7a97f25fbbd96c04e26b97f4548
xfourie6 <xfourie6@gmail.com>
2026-05-08 10:30:22 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import DupeshopLandingPage from "../dupeshop_za_landing_page.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <DupeshopLandingPage />
+);
