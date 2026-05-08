fb1e47dcc17b3d524da70fc4435cdd62a389f6bf
xfourie6 <xfourie6@gmail.com>
2026-05-08 14:02:36 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DupeshopLandingPage from '../dupeshop_za_landing_page'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DupeshopLandingPage />
-  </React.StrictMode>,
-)
