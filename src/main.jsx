c7e8bbdba3164f06ff1d8a929e0915550f8d8974
xfourie6 <xfourie6@gmail.com>
2026-05-08 13:58:35 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./style.css";
-import DupeshopLandingPage from "../dupeshop_za_landing_page.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <DupeshopLandingPage />
-);
